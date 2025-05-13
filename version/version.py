6da14d634ee078dad9c06faea9616b7207392018
AmandaAZ04 <amandakv2@hotmail.com>
2025-05-13 11:50:08 -0400

    aa

---
+++ version/version.py
@@ -0,0 +1,28 @@
+"""
+version MAJOR.MINOR.PATCH
+EJEMPLO v2.4.1
+
+MAJOR: (Version mayor): Se incrementa cuando se hacen cambios grandes (generalmente incompatibles)
+    con la version anterior.
+MINOR: (Version menor): Se incrementa cuando se agregan nuevas funcionalidades al sistema, pero
+    sin romper la compatibilidad (Desarrollo incremental)
+PATCH: (Parche o revision): Se incrementa, cuando se corrigen errores en el sistema, o mejoran 
+    funcionalidades (desarrollo en espiral)
+    
+"""
+
+# Historial
+#        15/04/2025 Inicio del desarrollo v1.0.0
+#        22/04/2025 Agrega opcion 5 Modificar cantidad v1.1.0
+#        22/04/2025 Mejora las funcionalidades de 3 y 4 al buscar con while v1.1.1
+#        29/04/2025 Cambio de paradigma, inicio el trabajo con funciones v2.0.0
+#        05/05/2025 Se reemplaza el buscar y mostrar producto por funciones y se
+#                   agrega control de keyboardInterrupt v2.0.1
+#        13/05/2025 Cambio de código a módulos v2.1.1
+
+#version = "v1.0.0"
+#version = "v1.1.0"
+#version = "v1.1.1"
+#version = "v2.0.0"
+#version = "v2.0.1"
+#version = "v2.1.1"
